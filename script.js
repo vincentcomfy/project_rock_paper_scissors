515f0789dc78b306bbf44b30e9f599a4e77f476e
vincent <85801240+vincentcomfy@users.noreply.github.com>
2025-09-02 23:11:51 +0800

    wrote pseudocode and function for getting player choice and tested against pseudocode parameters

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function computerChoice() {
         return "scissors";
     }
 }
+
+console.log(`The computer choice is ${computerChoice()}`);
+
+// Prompt the player to enter their choice via a prompt() window
+// Check their prompt is valid (i.e; is only one of 3 valid choices)
+// Test the prompt to ensure that only the allowed value is entered and incorrect values are thrown away
+
+function humanChoice() {
+    let choice = prompt("Please enter your choice; Rock, Paper or Scissors");
+    let cleaned = choice.toLowerCase().trim().replace(/[^a-z0-9]/g, "");
+    
+    if (cleaned === "rock") {
+        return "rock";
+    } else if (cleaned === "paper") {
+        return "paper";
+    } else if (cleaned === "scissors") {
+        return "scissors";
+    } else {
+        return "Invalid; Please enter either Rock, Paper or Scissors"
+    }
+}
+let playerChoice = humanChoice();
+console.log(`The human choice is ${playerChoice}`);
+
